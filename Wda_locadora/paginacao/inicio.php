0be9bf7840949e9cfaf6453edb7d211f1623e8bc
Emanoell Edvan <107198587+Emanoell-Edvan-S-Silva@users.noreply.github.com>
2023-05-15 07:55:24 -0300

    atualização 3.0

--- Wda_locadora/paginacao/inicio.php
+++ Wda_locadora/paginacao/inicio.php
@@ -80,7 +80,7 @@ $row4 = $cont2->fetch_array();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css-manual/pag.css">
+    <link rel="stylesheet" href="../css-manual/pag1.css">
     <link rel="shortcut icon" href="./imagens/book.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
